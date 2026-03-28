b0ae7ebd3cdea7bf03bc14b728e351db1937529e
JAYASREE-dev-creator <jayasreebabu2004@gmail.com>
2026-03-28 11:22:00 +0530

    Fix Cloudinary public access for PDFs

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,23 +1,18 @@
-const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: (req, file) => ({
-  folder: "skillhub_uploads",
-  resource_type: "auto",
-  type: "upload",   // 🔥 ADD THIS LINE
-  public_id: Date.now() + "-" + file.originalname,
-}),
+  params: (req, file) => {
+
+    let resourceType = "image"
+
+    if (file.mimetype === "application/pdf") {
+      resourceType = "raw"
+    }
+
+    return {
+      folder: "skillhub_uploads",
+      resource_type: resourceType,
+      type: "upload",
+      public_id: Date.now() + "-" + file.originalname,
+    }
+  },
 });
-
-const upload = multer({ storage });
-
-module.exports = upload;
